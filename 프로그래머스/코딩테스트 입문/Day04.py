8b4d0354720ba7eb0eeedc8cd968feb7d2c81dd0
Sumin Kim <102201066+smkim6927@users.noreply.github.com>
2023-07-22 19:53:46 +0900

    Create Day04.py

---
+++ 프로그래머스/코딩테스트 입문/Day04.py
@@ -0,0 +1,16 @@
+# 배열의 평균값
+
+def solution(numbers):
+    answer = 0
+    size = len(numbers)
+    res = sum(numbers)/size
+    print(res)
+    
+    if nam == 0 :
+        answer = res
+    elif nam == 5 :
+        answer = res
+    else :
+        answer = round(res,1)
+    
+    return answer

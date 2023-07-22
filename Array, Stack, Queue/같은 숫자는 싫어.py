feb5f3b397c7e19e7f6d25058e4aa82ee3530d08
Sumin Kim <102201066+smkim6927@users.noreply.github.com>
2023-07-22 18:03:15 +0900

    Create 같은 숫자는 싫어.py

---
+++ Array, Stack, Queue/같은 숫자는 싫어.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+   
+    for i in arr:
+        if i not in answer:
+            answer.append(i)
+        elif i != answer[-1]:
+            answer.append(i)
+        else:
+            pass
+    
+    return answer
